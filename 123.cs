7f61f54ca2af378ac9b4d6163baf672825c6fb6b
Andrusha1488 <145493233+Andrusha1488@users.noreply.github.com>
2024-01-28 03:04:59 +0300

    Add files via upload

---
+++ 123.cs
@@ -0,0 +1,19 @@
+﻿namespace 
+{
+    internal class Program
+    {
+        static int position = Switch.position, max = Switch.max, min = Switch.min;
+        static ConsoleKey key = Switch.key;
+        static string path = Base.path;
+        public static void Main(string[] args)
+        {
+            Menu(position, max, min, key, path);
+        }
+        static void Menu(int position, int max, int min, ConsoleKey key, string path)
+        {
+            min = 1; max = 2;
+            position = 1;
+            Base.Base2(key, position, max, min, path);
+        }
+    }
+}
